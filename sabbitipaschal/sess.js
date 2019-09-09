95eaa3a9e392812a08756d13940a5505839d4706
nyamarere1994 <paschalsabiti.gmail.com>
2019-09-09 16:10:02 +0300

    my first commit

---
+++ sabbitipaschal/sess.js
@@ -0,0 +1,79 @@
+
+
+const submit = document.getElementById("submit");
+
+compute.addEventListener("click", (event) => {
+
+   
+
+event.preventDefault();
+
+
+
+
+
+const surname = document.getElementById('surname');
+
+
+    const givenname = document.getElementById('givenname');
+
+
+        const sexm = document.getElementById('sexm');
+
+
+            const sexf = document.getElementById('sexf');
+
+
+                const dob = document.getElementById('dob');
+
+
+                    const country = document.getElementById('country');
+
+
+                    
+const por = document.getElementById('por');
+
+
+const phone = document.getElementById('phone');
+
+
+    const email = document.getElementById('email');
+
+
+
+
+
+
+
+
+
+
+if( !/[A-Za-z]/.test(surname) || surname.value ===""){
+
+
+
+ surname.style.border = "1px solid red";
+
+ document.getElementById('surfirst').style.visibility='visible';
+ 
+
+}
+
+})
+
+
+							
+                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
