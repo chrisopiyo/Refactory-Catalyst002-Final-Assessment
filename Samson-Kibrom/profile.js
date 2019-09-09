5bfb7c7b3d68f5c5331cdd590cce9d65d5bc79b1
[sam arsanios] <[ssamseat@hotmail.com]>
2019-09-09 18:33:44 +0300

    all the files

--- Samson-Kibrom/profile.js
+++ Samson-Kibrom/profile.js
@@ -22,7 +22,7 @@ const validate = () => {
       borderColour(surname, "1px solid red");
       warningMessage[0].innerHTML = "Surname is required";
       warningMessage[0].style =
-        "color: red; font-family: Helvetica, sans-serif;";
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
     } else {
       borderColour(surname, "1px solid rgb(122, 122, 139)");
       warningMessage[0].innerHTML = "";
@@ -32,20 +32,70 @@ const validate = () => {
       borderColour(givenname, "1px solid red");
       warningMessage[1].innerHTML = "Given name is required";
       warningMessage[1].style =
-        "color: red; font-family: Helvetica, sans-serif;";
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
     } else {
       borderColour(givenname, "1px solid rgb(122, 122, 139)");
       warningMessage[1].innerHTML = "";
     }
 
-    if (regexEmpty.test(surname.value) == true) {
+    if (regexEmpty.test(dob.value) == true) {
       borderColour(dob, "1px solid red");
-      warningMessage[0].innerHTML = "Surname is required";
-      warningMessage[0].style =
-        "color: red; font-family: Helvetica, sans-serif;";
+      warningMessage[2].innerHTML = "Date of birth is required";
+      warningMessage[2].style =
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
     } else {
-      borderColour(surname, "1px solid rgb(122, 122, 139)");
-      warningMessage[0].innerHTML = "";
+      borderColour(dob, "1px solid rgb(122, 122, 139)");
+      warningMessage[2].innerHTML = "";
+    }
+
+    if (regexEmpty.test(country.value) == true) {
+      borderColour(country, "1px solid red");
+      warningMessage[3].innerHTML = "Country Field is required";
+      warningMessage[3].style =
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
+    } else {
+      borderColour(country, "1px solid rgb(122, 122, 139)");
+      warningMessage[3].innerHTML = "";
+    }
+
+    if (regexEmpty.test(country.value) == true) {
+      borderColour(country, "1px solid red");
+      warningMessage[4].innerHTML = "Country Field is required";
+      warningMessage[4].style =
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
+    } else {
+      borderColour(country, "1px solid rgb(122, 122, 139)");
+      warningMessage[4].innerHTML = "";
+    }
+
+    if (regexEmpty.test(residence.value) == true) {
+      borderColour(residence, "1px solid red");
+      warningMessage[5].innerHTML = "This field is required";
+      warningMessage[5].style =
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
+    } else {
+      borderColour(residence, "1px solid rgb(122, 122, 139)");
+      warningMessage[5].innerHTML = "";
+    }
+
+    if (regexEmpty.test(phone.value) == true) {
+      borderColour(phone, "1px solid red");
+      warningMessage[6].innerHTML = "Country Field is required";
+      warningMessage[6].style =
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
+    } else {
+      borderColour(phone, "1px solid rgb(122, 122, 139)");
+      warningMessage[6].innerHTML = "";
+    }
+
+    if (regexEmpty.test(email.value) == true) {
+      borderColour(email, "1px solid red");
+      warningMessage[7].innerHTML = "Country Field is required";
+      warningMessage[7].style =
+        "color: red; font-family: Tahoma, Arial, sans-serif;";
+    } else {
+      borderColour(email, "1px solid rgb(122, 122, 139)");
+      warningMessage[7].innerHTML = "";
     }
   });
 };
