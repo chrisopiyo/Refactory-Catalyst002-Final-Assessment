afa740e5cfa91a6ce6fb4a953ec3cc77bd39453c
nanfuka <kalungi2k6@gmail.com>
2019-09-09 18:29:54 +0300

    Add more styling

--- deborahkalungi/myscript.js
+++ deborahkalungi/myscript.js
@@ -1,6 +1,11 @@
 // function to validate all the input fields
 function Validate() {
     surname = document.getElementById("surname");
+    givenname = document.getElementById("givenname");
+    dateofbirth = document.getElementById("dateofbirth");
+    country = document.getElementById("country");
+    // function which validates surname to ensure that 
+    // its not left blank and elements entered are only characters from a-z.
     const validSurname = /^[A-Za-z]+$/;
     if (surname.value.match(validSurname)) {
         surname.style.border = "2px green solid";
@@ -12,7 +17,40 @@ function Validate() {
         surname.style.border = "2px red solid";
         return false;
     }
-    console.log("connected")
+
+    // function which validates surname to ensure that 
+    // its not left blank and elements entered are only characters from a-z.
+    const valid_givenname = /^[A-Za-z]+$/;
+    if (givenname.value.match(valid_givenname)) {
+        givenname.style.border = "2px green solid";
+        document.getElementById("given_name_error").style.display = "none";
+
+    }
+    else {
+        document.getElementById("given_name_error").innerHTML = "Given name is required"
+        givenname.style.border = "2px red solid";
+        return false;
+    }
+
+
+    // function which validates Date of birth to ensure that 
+    // its not left blank.
+
+    const valid_date = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/;
+    if (dateofbirth.value.match(valid_date)) {
+        dateofbirth.style.border = "2px green solid";
+        document.getElementById("date_error").style.display = "none";
+
+    }
+    else {
+        document.getElementById("date_error").innerHTML = "date of bith is required in the formart specified in the input"
+        dateofbirth.style.border = "2px red solid";
+        return false;
+    }
+
+     // function which validates country
+
+
 }
-// Validate();
+
 
