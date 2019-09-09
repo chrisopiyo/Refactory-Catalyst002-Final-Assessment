6ab658f14ca4178bb38ce3b0a361fc3d3c15e73c
ankundn <ankundanisha0@gmail.com>
2019-09-09 17:38:16 +0300

    Fifth Commit

--- Ankunda Nisha/form.js
+++ Ankunda Nisha/form.js
@@ -1,10 +1,14 @@
+/* Ankunda Nisha Stephanie
+validate function to ensure that the form is
+ submitted when all the required inputs are entered*/
+
 const validate = () => {
     // adding an event listener so that the alert message styles can be seen after an alert is read
     document.getElementById('form').addEventListener('submit', (e) => {
         e.preventDefault();
     });
 
-//validation so that surname field isnt empty
+    //validation so that surname field isnt empty
     const namev = document.getElementById('sur');
     const alpha = /^[A-Za-z]+$/;
     if (namev.value.match(alpha) || namev == '') {
@@ -28,51 +32,51 @@ const validate = () => {
 
     //validation so that date of birth is given
     const birth = document.getElementById('date');
-    if(birth.value == ''){
+    if (birth.value == '') {
         alert("This field is required");
         birth.style.border = "2px red solid";
     }
 
     //validation so that country field isn't empty
     const city = document.getElementById('country');
-   if(city.value == '----select country-----'){
-    alert('Please choose a country');
-    city.style.border = "2px red solid";
-   }
-   else{
-       return true;
-   }
- 
-   //validation so that place of resience field isn't empty
+    if (city.value == '----select country-----') {
+        alert('Please choose a country');
+        city.style.border = "2px red solid";
+    }
+    else {
+        return true;
+    }
 
-   const pla = document.getElementById('place');
-   if(pla.value == ''){
-       alert('This field is required');
-       pla.style.border = "2px red solid";
-   }
-   
-   //validation so that phone number field isn't empty
-   const zip = document.getElementById('num');
-   const code = /^[-+]?[0-9]+$/;
-   if (zip.value == '' || zip.value.match(code)) {
-       alert('This field is required');
-       zip.style.border = "2px red solid";
-   }
-   //email validation
-   const mail = document.getElementById('email');
-   const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-   if (mail.value.match(mailformat) || mail.value == ' ') {
-       return true;
-   }
-   else {
-       alert('This field is required, a valid email has "@"');
-       mail.style.border = "2px red solid";
-   }
+    //validation so that place of resience field isn't empty
+
+    const pla = document.getElementById('place');
+    if (pla.value == '') {
+        alert('This field is required');
+        pla.style.border = "2px red solid";
+    }
+
+    //validation so that phone number field isn't empty
+    const zip = document.getElementById('num');
+    const code = /^[-+]?[0-9]+$/;
+    if (zip.value == '' || zip.value.match(code)) {
+        alert('This field is required');
+        zip.style.border = "2px red solid";
+    }
+    //email validation
+    const mail = document.getElementById('email');
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (mail.value.match(mailformat) || mail.value == ' ') {
+        return true;
+    }
+    else {
+        alert('This field is required, a valid email has "@"');
+        mail.style.border = "2px red solid";
+    }
 
 
 }
 
-
+//function to enable a user leave the page when they click exit
 
 const leave = () => {
 
